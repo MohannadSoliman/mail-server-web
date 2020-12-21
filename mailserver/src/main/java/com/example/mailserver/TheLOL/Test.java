c6d0558476ba9cdadb4c512fb84a0edc68fde251
MohannadSoliman <61092652+MohannadSoliman@users.noreply.github.com>
2020-12-21 21:56:56 +0200

    Update Test.java

--- mailserver/src/main/java/com/example/mailserver/TheLOL/Test.java
+++ mailserver/src/main/java/com/example/mailserver/TheLOL/Test.java
@@ -1,18 +1,24 @@
 package com.example.mailserver.TheLOL;
 
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Test {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Queue<String> receivers = new LinkedList<String>();
         Queue<String> attachments = new LinkedList<String>();
         receivers.add("testREC@g");
 
-       Email email = new Email(null, "test@g", receivers, "titleLOLtimeTest", "bodyLOL", null, attachments);
+        Email email = new Email(null, "test@g", receivers, "titleLOLtimeTest", "bodyLOL", null, attachments);
         User user = new User("test@g");
 
-        user.sendEmail(email);
+        // user.sendEmail(email);
         // user.deleteEmail("7bc7bb58-99bb-4db2-b464-80be7a52afe1", "sent");
+        // try {
+        //     user.createNewCustomFolder("testFolder");
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
     }
 }
